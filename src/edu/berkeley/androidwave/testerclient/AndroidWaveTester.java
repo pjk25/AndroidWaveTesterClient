9fae99a88b88f69763d5248626a46bb2b5f148ee
Philip Kuryloski <pjk25@cornell.edu>
2011-06-21 14:47:10 -0700

    Initial commit
    Base project created with 'android create project'

---
+++ src/edu/berkeley/androidwave/testerclient/AndroidWaveTester.java
@@ -0,0 +1,15 @@
+package edu.berkeley.androidwave.testerclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidWaveTester extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
